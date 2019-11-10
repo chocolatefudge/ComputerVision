8bc66493663a6c8358bae60117c706753b3665ac
ksa37 <rlatndk010@naver.com>
2019-11-10 23:28:13 +0100

    problem3_task2 done

--- assignment1/problem3.py
+++ assignment1/problem3.py
@@ -2,21 +2,26 @@ import numpy as np
 
 def load_points(path):
     '''
-    Load points from path pointing to a numpy binary file (.npy). 
+    Load points from path pointing to a numpy binary file (.npy).
     Image points are saved in 'image'
     Object points are saved in 'world'
 
     Returns:
-        image: A Nx2 array of 2D points form image coordinate 
+        image: A Nx2 array of 2D points form image coordinate
         world: A N*3 array of 3D points form world coordinate
     '''
 
+
     #idk part how to load the points from npz file
-    image_pts = np.empty((100, 3))
-    world_pts = np.empty((100, 4))
+    data = np.load('data/points.npz') # loading data
+    lst = data.files # It includes the key image and world
+
+    image_pts = data[lst[0]]#np.empty((100, 3))
+    world_pts = data[lst[1]]#np.empty((100, 4))
 
     # sanity checks
     assert image_pts.shape[0] == world_pts.shape[0]
+    # As I checked, they're both 75
 
     # homogeneous coordinates
     assert image_pts.shape[1] == 3 and world_pts.shape[1] == 4
@@ -26,35 +31,36 @@ def load_points(path):
 def create_A(x, X):
     """Creates (2*N, 12) matrix A from 2D/3D correspondences
     that comes from cross-product
-    
+
     Args:
         x and X: N 2D and 3D point correspondences (homogeneous)
-        
+
     Returns:
         A: (2*N, 12) matrix A
     """
 
     N, _ = x.shape
-    assert N == X.shape[0]
+    assert N == X.shape[0] #N is 75
+    print(N)
 
-    A = np.empty((200, 12))
+    A = np.empty((150, 12))
 
     for i in range(N):
         x1 = x[i][0]
         y1 = x[i][1]
         temp = X[i]
 
-        if i%2:
-            A[0:4] = 0
-            A[4:8] = -np.transpose(temp)
-            A[8:12] = y1*np.transpose(temp)
+        #print(A[0:4])
 
-        else:
-            A[0:4] = np.transpose(temp)
-            A[4:8] = 0
-            A[8:12] = -x1*np.transpose(temp)
+        A[2*i,0:4] = 0
+        A[2*i,4:8] = -np.transpose(temp)
+        A[2*i,8:12] = y1*np.transpose(temp)
 
-    
+        A[2*i+1,0:4] = np.transpose(temp)
+        A[2*i+1,4:8] = 0
+        A[2*i+1,8:12] = -x1*np.transpose(temp)
+
+
     assert A.shape[0] == 2*N and A.shape[1] == 12
     return A
 
@@ -65,7 +71,7 @@ def homogeneous_Ax(A):
 
     Args:
         A: (2*N, 12) matrix A
-    
+
     Returns:
         P: (3, 4) projection matrix P
     """
@@ -77,20 +83,20 @@ def homogeneous_Ax(A):
 
 
 def solve_KR(P):
-    """Using th RQ-decomposition find K and R 
+    """Using th RQ-decomposition find K and R
     from the projection matrix P.
     Hint 1: you might find scipy.linalg useful here.
     Hint 2: recall that K has 1 in the the bottom right corner.
     Hint 3: RQ decomposition is not unique (up to a column sign).
-    Ensure positive element in K by inverting the sign in K columns 
+    Ensure positive element in K by inverting the sign in K columns
     and doing so correspondingly in R.
 
     Args:
         P: 3x4 projection matrix.
-    
+
     Returns:
         K: 3x3 matrix with intrinsics
-        R: 3x3 rotation matrix 
+        R: 3x3 rotation matrix
     """
 
     M = np.hsplit(P, np.array([3,6]))[0]
@@ -108,7 +114,7 @@ def solve_c(P):
 
     Args:
         P: 3x4 projection matrix
-    
+
     Returns:
         c: 3x1 camera center coordinate in the world frame
     """
