298a01639fddc4622ffa03456cb3595967ef2d64
ksa37 <rlatndk010@naver.com>
2020-01-19 03:55:43 +0100

    problem2 done comment needed

--- assignment4/problem2.py
+++ assignment4/problem2.py
@@ -2,39 +2,37 @@ import numpy as np
 
 def cost_ssd(patch1, patch2):
     """Compute the Sum of Squared Pixel Differences (SSD):
-    
+
     Args:
         patch1: input patch 1 as (m, m, 1) numpy array
         patch2: input patch 2 as (m, m, 1) numpy array
-    
+
     Returns:
         cost_ssd: the calcuated SSD cost as a floating point value
     """
 
-    #
-    # Your code goes here
-    #
-    cost_ssd = -1
-
+    cost_ssd = ((patch1-patch2)**2).sum()
     assert np.isscalar(cost_ssd)
     return cost_ssd
 
 
 def cost_nc(patch1, patch2):
     """Compute the normalized correlation cost (NC):
-    
+
     Args:
         patch1: input patch 1 as (m, m, 1) numpy array
         patch2: input patch 2 as (m, m, 1) numpy array
-    
+
     Returns:
         cost_nc: the calcuated NC cost as a floating point value
     """
-
-    #
-    # Your code goes here
-    #
-    cost_nc = -1
+    m = patch1.shape[0]
+    repatch1 = np.reshape(patch1, (m**2,1))
+    repatch2 = np.reshape(patch2, (m**2,1))
+    w1_mean = np.mean(repatch1)
+    w2_mean = np.mean(repatch2)
+    cost_nc = np.dot(np.transpose(repatch1-w1_mean),repatch2-w2_mean)[0][0]
+    cost_nc /= np.linalg.norm(repatch1-w1_mean)*np.linalg.norm(repatch2-w2_mean)
 
     assert np.isscalar(cost_nc)
     return cost_nc
@@ -42,51 +40,52 @@ def cost_nc(patch1, patch2):
 
 def cost_function(patch1, patch2, alpha):
     """Compute the cost between two input window patches given the disparity:
-    
+
     Args:
         patch1: input patch 1 as (m, m) numpy array
         patch2: input patch 2 as (m, m) numpy array
-        input_disparity: input disparity as an integer value        
+        input_disparity: input disparity as an integer value
         alpha: the weighting parameter for the cost function
     Returns:
         cost_val: the calculated cost value as a floating point value
     """
-    assert patch1.shape == patch2.shape 
 
-    #
-    # Your code goes here
-    #
-    cost_val = -1
-    
+    assert patch1.shape == patch2.shape
+    m = patch1.shape[0]
+    cost_val = cost_ssd(patch1, patch2)/(m**2) + alpha*cost_nc(patch1, patch2)
+
     assert np.isscalar(cost_val)
     return cost_val
 
 
-def pad_image(input_img, window_size, padding_mode='symmetric'):
+def pad_image(input_img, window_size, padding_mode):
     """Output the padded image
-    
+
     Args:
         input_img: an input image as a numpy array
         window_size: the window size as a scalar value, odd number
         padding_mode: the type of padding scheme, among 'symmetric', 'reflect', or 'constant'
-        
+
     Returns:
         padded_img: padded image as a numpy array of the same type as image
     """
     assert np.isscalar(window_size)
     assert window_size % 2 == 1
 
-    #
-    # Your code goes here
-    #
-    padded_img = input_img.copy()
+    pad_width = int((window_size-1)/2)
+    if padding_mode == 'symmetric':
+        padded_img = np.pad(input_img, pad_width, padding_mode)
+    elif padding_mode == 'reflect':
+        padded_img = np.pad(input_img, pad_width, padding_mode)
+    elif padding_mode == 'constant':
+        padded_img = np.pad(input_img, pad_width, padding_mode)
 
     return padded_img
 
 
 def compute_disparity(padded_img_l, padded_img_r, max_disp, window_size, alpha):
-    """Compute the disparity map by using the window-based matching:    
-    
+    """Compute the disparity map by using the window-based matching:
+
     Args:
         padded_img_l: The padded left-view input image as 2-dimensional (H,W) numpy array
         padded_img_r: The padded right-view input image as 2-dimensional (H,W) numpy array
@@ -97,50 +96,65 @@ def compute_disparity(padded_img_l, padded_img_r, max_disp, window_size, alpha):
         disparity: numpy array (H,W) of the same type as image
     """
 
-    assert padded_img_l.ndim == 2 
-    assert padded_img_r.ndim == 2 
+    assert padded_img_l.ndim == 2
+    assert padded_img_r.ndim == 2
     assert padded_img_l.shape == padded_img_r.shape
     assert max_disp > 0
     assert window_size % 2 == 1
 
-    #
-    # Your code goes here
-    #
-    disparity = padded_img_l.copy()
+    origin_m = padded_img_l.shape[0]-window_size+1
+    origin_n = padded_img_l.shape[1]-window_size+1
+    disparity = np.zeros((origin_m, origin_n))
+    for i in range(origin_m):
+        for j in range(origin_n):
+            patch1 = padded_img_l[i:i+window_size, j:j+window_size]
+            min_cost = None
+            min_disp = None
+            for k in range(-15, 16):
+                if j-k<0:
+                    continue
+                if j-k+window_size> padded_img_r.shape[1]:
+                    continue
+                patch2 = padded_img_r[i:i+window_size, j-k:j+window_size-k]
+                cost = cost_function(patch1, patch2, alpha)
+                if min_cost == None:
+                    min_cost = cost
+                    min_disp = k
+                elif cost < min_cost:
+                    min_cost = cost
+                    min_disp = k
+            disparity[i,j] = min_disp
 
     assert disparity.ndim == 2
     return disparity
 
 def compute_aepe(disparity_gt, disparity_res):
     """Compute the average end-point error of the estimated disparity map:
-    
+
     Args:
         disparity_gt: the ground truth of disparity map as (H, W) numpy array
         disparity_res: the estimated disparity map as (H, W) numpy array
-    
+
     Returns:
         aepe: the average end-point error as a floating point value
     """
-    assert disparity_gt.ndim == 2 
-    assert disparity_res.ndim == 2 
+    assert disparity_gt.ndim == 2
+    assert disparity_res.ndim == 2
     assert disparity_gt.shape == disparity_res.shape
 
-    #
-    # Your code goes here
-    #
-    aepe = -1
+    size = disparity_gt.shape[0]*disparity_gt.shape[1]
+
+    aepe = np.sum(np.absolute(disparity_gt-disparity_res))/size
 
     assert np.isscalar(aepe)
     return aepe
 
 def optimal_alpha():
-    """Return alpha that leads to the smallest EPE 
+    """Return alpha that leads to the smallest EPE
     (w.r.t. other values)"""
-    
-    #
-    # Fix alpha
-    #
-    alpha = np.random.choice([-0.06, -0.01, 0.04, 0.1])
+
+    alpha = -0.01
+    # np.random.choice([-0.06, -0.01, 0.04, 0.1])
     return alpha
 
 
@@ -150,14 +164,14 @@ This is a multiple-choice question
 class WindowBasedDisparityMatching(object):
 
     def answer(self):
-        """Complete the following sentence by choosing the most appropriate answer 
+        """Complete the following sentence by choosing the most appropriate answer
         and return the value as a tuple.
         (Element with index 0 corresponds to Q1, with index 1 to Q2 and so on.)
-        
-        Q1. [?] is better for estimating disparity values on sharp objects and object boundaries
+
+        Q1. [?] is better for estimating disparity values on sharp objects and object boundaries.
           1: Using a smaller window size (e.g., 3x3)
           2: Using a bigger window size (e.g., 11x11)
-        
+
         Q2. [?] is good for estimating disparity values on locally non-textured area.
           1: Using a smaller window size (e.g., 3x3)
           2: Using a bigger window size (e.g., 11x11)
@@ -175,4 +189,4 @@ class WindowBasedDisparityMatching(object):
 
         """
 
-        return (-1, -1, -1, -1)
+        return (1, 2, 1, 2)
